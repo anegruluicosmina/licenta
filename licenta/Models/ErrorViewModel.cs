4f4d913e36b9d1be0b2ad321e5fc0d5e5df9bfe3
MARIA-COSMINA ANEGRULUI <maria.anegrului@stud.ubbcluj.ro>
2021-02-22 15:16:25 +0200

    Add project files.

---
+++ licenta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace licenta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
